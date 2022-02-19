d458fe362112994404f09d7324a4ae2300e144b7
Andrés Barros Rivas <andresbr92@gmail.com>
2022-02-19 19:14:44 +0100

    new function

--- functions.sol
+++ functions.sol
@@ -3,12 +3,18 @@ pragma solidity >0.7.0 <0.9.0;
 
 
 contract Functions {
+
 	function Sum(uint num1, uint num2) public pure returns(uint) {
+		return sumIntern(num1, num2);
+	}
+
+	function sumIntern (uint num1, uint num2) private pure returns(uint) {
 		return num1 + num2;
 	}
+
 	uint private result;
+
 	function ObtainResult() public view returns(uint) {
 		return result;
-
 	}
 }
