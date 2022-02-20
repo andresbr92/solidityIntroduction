96529b0cdda61386f58865dba0bddc23c3d7862c
Andrés Barros Rivas <andresbr92@gmail.com>
2022-02-20 16:09:13 +0100

    first steps reception

--- Reception.sol
+++ Reception.sol
@@ -3,4 +3,15 @@ pragma solidity >=0.7.0 <0.9.0;
 
 	
 contract Reception {
+	mapping(address => uint) balances;
+
+	receive() external payable {
+		balances[msg.sender] += msg.value;
+	}
+	fallback() external payable {
+	}
+	function receiveBalance(uint number) public payable {
+		uint quantity = msg.value;
+		quantity = number; 
+	} 
 }
