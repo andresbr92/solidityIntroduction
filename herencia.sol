7f3a6627edb1a6e5f1f51779209be112ae9d673f
Andrés Barros Rivas <andresbr92@gmail.com>
2022-02-20 16:37:12 +0100

    first steps with inheritance

--- herencia.sol
+++ herencia.sol
@@ -1,6 +1,17 @@
 // SPDX-license-identifier: GPL-2.0-or-later
 pragma solidity >=0.7.0 <0.9.0;
 
-	
-contract Herencia {
+import "./inteface.sol";
+import "./modifiersHerencia.sol";
+
+contract Herencia is Sum, Modifiers {
+
+    constructor(string memory newName) Modifiers(newName) {
+        
+    }
+	function sum(uint num1, num2) public override IsOwner() view returns(uint) {
+		return num1 + num2;
+	}
+
+
 }
